e013f26c937fa9beb0110170a50c5979d94cfdb4
asjadxz <asjadk224@gmail.com>
2024-05-31 12:17:43 +0500

    Add files via upload

---
+++ typescript 45 questions/2.personal message.ts
@@ -0,0 +1,7 @@
+// Store a person’s name in a variable, and print a message to that person. 
+let personName = "aseef";
+
+//Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+console.log(`Hello ${personName}, would u like to learn some python today?`);
+
+
